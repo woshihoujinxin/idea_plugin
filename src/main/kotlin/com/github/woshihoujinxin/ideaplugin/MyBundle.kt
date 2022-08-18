3d2f6338d416525aa606b03e6189d51b17c77cba
GitHub Action <action@github.com>
2022-08-18 12:40:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/woshihoujinxin/ideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/woshihoujinxin/ideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.woshihoujinxin.ideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/woshihoujinxin/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/woshihoujinxin/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.woshihoujinxin.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.woshihoujinxin.ideaplugin.MyBundle
 
 class MyApplicationService {
 
